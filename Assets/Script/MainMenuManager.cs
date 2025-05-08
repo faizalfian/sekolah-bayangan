60bed35384eeb8aaa31331ee8708cad82ea50389
Azaria Adila Putri <156308576+starvann@users.noreply.github.com>
2025-05-07 19:21:07 -0700

    UI

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -3,14 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-   public void Play()
-   {
-      Debug.Log("Clicked");
-    SceneManager.LoadScene("SekolahBayangan-GamePlay");
-   }
+    public void Play()
+    {
+        Debug.Log("Play clicked");
+        SceneManager.LoadScene("SekolahBayangan-GamePlay");
+    }
 
-   public void Exit()
-   {
-    Application.Quit();
-   }
+    public void Exit()
+    {
+        Debug.Log("Exit clicked");
+        Application.Quit();
+    }
 }
