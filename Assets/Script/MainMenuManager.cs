0e9d3a204d5a46eacfed3ad34df16497ece96209
faiz-dev <68036226+faiz-dev@users.noreply.github.com>
2025-05-09 10:50:41 +0700

    AI Attack jalan

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -6,7 +6,7 @@ public class MainMenuManager : MonoBehaviour
     public void Play()
     {
         Debug.Log("Play clicked");
-        SceneManager.LoadScene("SekolahBayangan-GamePlay");
+        SceneManager.LoadScene("CobaStage1");
     }
 
     public void Exit()
